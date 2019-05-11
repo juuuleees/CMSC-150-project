98d5764d1fc87a514ea2c547dc41baccddaae1bd
Julianne Ruz <jaruz@up.edu.ph>
2019-05-11 23:15:34 +0800

    Improved UI.

--- server.r
+++ server.r
@@ -1,18 +1,20 @@
 library(shiny)
-library(datasets)
+library(rhandsontable)
 
 shinyServer(function(input, output) {
+
+	output$value <- renderText({ input$x_value })
 	
-	observeEvent(input$polyreg, {
-		print("do polynomial regression")
-	})
+	# observeEvent(input$polyreg, {
+	# 	print("do polynomial regression")
+	# })
 
-	observeEvent(input$quadspline, {
-		print("do quadratic spline")
-	})
+	# observeEvent(input$quadspline, {
+	# 	print("do quadratic spline")
+	# })
 
-	observeEvent(input$simplex, {
-		print("do simplex")
-	})
+	# observeEvent(input$simplex, {
+	# 	print("do simplex")
+	# })
 
 })
--- ui.r
+++ ui.r
@@ -1,21 +1,89 @@
 library(shiny)	
+library(rhandsontable)
 
-shinyUI(pageWithSidebar(
-	
-	headerPanel("CSV Reader"),
+fluidPage(
+	tabsetPanel(
+		tabPanel(
+			title = "Polynomial Regression",
+			sidebarLayout(
+				sidebarPanel(
+					fileInput(
+						"poly_reg_file",
+						"Choose CSV file",
+						accept = c('text/c', '.csv')
+					),
+					tags$hr(),
+					textInput(
+						"order",
+						"Order value",
+						"",
+						placeholder = "e.g. input '4' for 4th-order polynomials"
+					),
+					textInput(
+						"x_value",
+						"Value of x",
+						""
+					),
+					actionButton(
+						"pr_solve",
+						"Solve"
+					)
+				),
+				mainPanel(
 
-	sidebarPanel(
+				)
+			)
+		),
+		tabPanel(
+			title = "Quadratic Spline",
+			sidebarLayout(
+				sidebarPanel(
+					fileInput(
+						"quad_spline_file",
+						"Choose CSV file",
+						accept = c('text/c', '.csv')
+					),
+					tags$hr(),
+					textInput(
+						"x_value",
+						"Value of x",
+						""
+					),
+					actionButton(
+						"qs_solve",
+						"Solve"
+					)
+				),
+				mainPanel(
 
-		fileInput('file1', "Choose CSV file", accept = c('text/c', '.csv')),
-		tags$hr(),
-		actionButton("polyreg", "Polynomial Regression"),
-		actionButton("quadspline", "Quadratic Spline"),
-		actionButton("simplex", "Simplex Method")
-
-	),
-
-	mainPanel(
-		
+				)
+			)	
+		),
+		tabPanel(
+			title = "Simplex"
+		)
 	)
+)
 
-))
+# shinyUI(pageWithSidebar(
+	
+	# headerPanel("CSV Reader"),
+
+	# sidebarPanel(
+
+	# 	fileInput('file1', "Choose CSV file", accept = c('text/c', '.csv')),
+	# 	tags$hr(),
+	# 	actionButton("polyreg", "Polynomial Regression"),
+	# 	actionButton("quadspline", "Quadratic Spline"),
+	# 	actionButton("simplex", "Simplex Method")
+
+	# 	# textInput("caption", "Caption", "Data Summary"),
+	# 	# verbatimTextOutput("value"),
+
+	# ),
+
+	# mainPanel(
+
+	# )
+
+# ))
